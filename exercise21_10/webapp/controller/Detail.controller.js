04b73d0a67a80d5d766eefe1ba1e2c997e1137d1
YeRamJung <jyr2356@naver.com>
2025-03-14 18:29:52 +0900

    2025.03.14.18:29

--- exercise21_10/webapp/controller/Detail.controller.js
+++ exercise21_10/webapp/controller/Detail.controller.js
@@ -1,24 +1,51 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  (Controller, JSONModel) => {
+    "use strict";
 
-  return Controller.extend("sync6.exercise2110.controller.Detail", {
-    onInit() {
-      this.oRouter = this.getOwnerComponent().getRouter(); // 컨트롤러에 유효한 객체
-    },
+    return Controller.extend("sync6.exercise2110.controller.Detail", {
+      onInit() {
+        this.oRouter = this.getOwnerComponent().getRouter(); // 컨트롤러에 유효한 객체
 
-    //이전 화면으로 돌아가는 함수
-    onGoBack() {
-      this.oRouter.navTo(
-        "RouteMain",
-        {
-          "?query": {
-            keyA: "ToMain",
-            keyB: "abc",
-            keyC: "haha",
-          },
-        },
-        true
-      );
-    },
-  });
-});
+        //데이터 뽑아오기
+        this.oRouter
+          .getRoute("RouteDetail")
+          .attachPatternMatched(this.onPatternMatched, this);
+      },
+
+      // manifest에 정의해둔 pattern이 일치할 때마다 실행되는 이벤트
+      onPatternMatched(oEvent) {
+        // 초기화, 셋팅, 매개변수 찾기 - 데이터 뽑아오기
+        // manifest에 정의해둔 라우터를 가져오고, 그 라우터에 정의된 파라미터 인자들 가져오기 & 전달할 값들 구분하기
+
+        //1. URL 에서 전달된 파라미터 가져오기.
+        // var sId = oEvent.getParameter("arguments").key1; //키 지정
+        var oArgu = oEvent.getParameters().arguments; //배열
+        // this.oArgu = oEvent.getParameters().arguments;
+
+        // console.log(oArgu);
+
+        //2. 새로운 JSON 모델 생성하여 ID 값을 저장
+        // var oModel = new JSONModel({ key1: sId });
+        var oModel = new JSONModel(oArgu);
+
+        //3. 뷰에 "detailModel"로 모델 설정
+        this.getView().setModel(oModel, "detailModel");
+      },
+
+      //메인 화면으로 돌아가는 함수
+      onGoBack() {
+        this.oRouter.navTo("RouteMain", {}, true);
+      },
+    });
+  }
+);
+
+// {
+//   "?query": {
+//     no: this.oArgu.no,
+//     name: this.oArgu.name,
+//     gender: this.oArgu.gender,
+//     birthdate: this.oArgu.birthdate,
+//   },
+// }
--- exercise21_10/webapp/controller/Main.controller.js
+++ exercise21_10/webapp/controller/Main.controller.js
@@ -1,22 +1,54 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  (Controller, MessageToast) => {
+    "use strict";
 
-  return Controller.extend("sync6.exercise2110.controller.Main", {
-    onInit() {
-      this.oRouter = this.getOwnerComponent().getRouter(); // 컨트롤러에 유효한 객체
-    },
-    onGoDetail() {
-      //input 필드에 들어온 값 찾기
-      var sValue = this.getView().byId("input").getValue();
+    return Controller.extend("sync6.exercise2110.controller.Main", {
+      // 컨트롤러가 load될 때, 한번만 실행 - 모델을 생성함
+      onInit() {
+        //한 단계 위에 있는 Component에 접근해서 라우터를 가져옴
+        this.oRouter = this.getOwnerComponent().getRouter(); // 컨트롤러에 유효한 객체
+      },
 
-      this.oRouter.navTo(
-        "RouteDetail",
-        {
-          key1: sValue,
-          key2: "hello",
-        },
-        true
-      );
-    },
-  });
-});
+      //버튼 클릭 시, 값 전달
+      onGoDetail() {
+        //input 필드에 들어온 값 찾기
+        // var sValue = this.getView().byId("input").getValue();
+        // this.oRouter.navTo(
+        //   "RouteDetail",
+        //   {
+        //     key1: sValue,
+        //     key2: "hello",
+        //   },
+        //   true
+        // );
+
+        // 테이블 가져오기
+        var oTable = this.getView().byId("studentTable");
+
+        var oSelectItem = oTable.getSelectedItem();
+
+        //선택된 행이 없는 경우
+        if (!oSelectItem) {
+          MessageToast.show("학생 정보를 선택하세요");
+          return;
+        }
+
+        var oData = oSelectItem.getBindingContext("studentModel").getObject();
+        console.log(oData);
+
+        //선택된 학생의 no(아이디)를 Detail.view 쪽으로 라우팅
+        this.oRouter.navTo(
+          "RouteDetail",
+          {
+            no: oData.no,
+            name: oData.name,
+            gender: oData.gender,
+            birthdate: oData.birthdate,
+          },
+          true
+        );
+      },
+    });
+  }
+);
